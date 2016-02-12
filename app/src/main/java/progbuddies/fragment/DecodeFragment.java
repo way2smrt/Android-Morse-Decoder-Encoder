f64253971a22412e8a96002693e67359b1d741e3
Bilal Tahir <bilal.tahir1323@gmail.com>
2016-02-11 20:00:14 -0500

    Changed the EditText in decode fragment to TextView Quick implementation of button functionality

--- app/src/main/java/progbuddies/fragment/DecodeFragment.java
+++ app/src/main/java/progbuddies/fragment/DecodeFragment.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import progbuddies.activity.R;
+import progbuddies.morsecode.C;
 import progbuddies.morsecode.Decoder;
 
 /**
@@ -18,22 +19,103 @@ import progbuddies.morsecode.Decoder;
 public class DecodeFragment extends android.support.v4.app.Fragment {
 
     Decoder decoder;
-    EditText editText;
-    Button button;
-    TextView textView;
+    TextView textWindow;
+    Button dotButton;
+    Button dashButton;
+    Button deleteButton;
+    Button nextWordButton;
+    Button nextCharacterButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_decode, container, false);
-
         decoder = new Decoder();
+        textWindow = (TextView) view.findViewById(R.id.edit_text);
+
+        dotButton = (Button)view.findViewById(R.id.dotButton);
+
+        dotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dot(v);
+            }
+        });
+
+        dashButton = (Button) view.findViewById(R.id.dashButton);
+
+        dashButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dash(v);
+            }
+        });
+
+        deleteButton = (Button) view.findViewById(R.id.delete);
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delete(v);
+            }
+        });
+
+        nextCharacterButton = (Button) view.findViewById(R.id.nextCharacterButton);
+
+        nextCharacterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextCharacterButton(v);
+            }
+        });
+
+        nextWordButton = (Button) view.findViewById(R.id.nextWordButton);
+
+        nextWordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextWord(v);
+            }
+        });
 
         return view;
     }
 
-    public void decodeText(View view) {
-        String encodedText = editText.getText().toString().toLowerCase();
-        String decodedText = decoder.decode(encodedText);
-        textView.setText(decodedText);
+
+
+    public void dot(View view){
+        String current = textWindow.getText().toString();
+        String updated = current.concat(".");
+        textWindow.setText(updated);
     }
+
+    public void dash(View view) {
+        String current = textWindow.getText().toString();
+        String updated = current.concat("-");
+        textWindow.setText(updated);
+    }
+
+
+    public void nextCharacterButton(View view) {
+        String current = textWindow.getText().toString();
+        String updated = current.concat(" ");
+        textWindow.setText(updated);
+    }
+
+    public void nextWord(View view) {
+        String current = textWindow.getText().toString();
+        String updated = current.concat(C.WORD_SEPERATOR);
+        textWindow.setText(updated);
+    }
+
+
+    //TODO: This will throw an error, this is a quick implementation to test it, add proper check
+
+    public void delete(View view) {
+        String current = textWindow.getText().toString();
+        String updated = textWindow.getText().toString().substring(0, current.length()-1);
+        updated = updated.trim();
+        textWindow.setText(updated);
+    }
+
+
 }
